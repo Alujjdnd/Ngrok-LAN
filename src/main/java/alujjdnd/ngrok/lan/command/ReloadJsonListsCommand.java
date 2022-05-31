f0dc40acfa33a9b2c075e2d116587cc738300c1f
Flooflez <maxmyta@gmail.com>
2022-05-31 21:28:05 +0800

    Update ReloadJsonListsCommand.java
    TODO:
    REGISTER COMMAND ON SERVER/CLIENT INIT
    
    ENSURE JSON FILE IS BEING READ ON NGROKLAN START UP (with debug message in chat/logs)

--- src/main/java/alujjdnd/ngrok/lan/command/ReloadJsonListsCommand.java
+++ src/main/java/alujjdnd/ngrok/lan/command/ReloadJsonListsCommand.java
@@ -3,12 +3,16 @@ package alujjdnd.ngrok.lan.command;
 import alujjdnd.ngrok.lan.NgrokLan;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.argument.GameProfileArgumentType;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.ServerConfigHandler;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.TranslatableText;
 
 public class ReloadJsonListsCommand {
 
@@ -16,34 +20,28 @@ public class ReloadJsonListsCommand {
     //run loadJson();
     //get source.getServer() from command
 
+    private static final SimpleCommandExceptionType LOAD_JSON_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("text.info.ngroklan.reload.message"));
+
     public ReloadJsonListsCommand() {
     }
 
     //TODO: this is literally /op rn just need to change it
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder) CommandManager.literal("op").requires((source) -> {
-            return source.hasPermissionLevel(3);
-        })).then(CommandManager.argument("targets", GameProfileArgumentType.gameProfile()).suggests((context, builder) -> {
-            PlayerManager playerManager = (context.getSource()).getServer().getPlayerManager();
-            return CommandSource.suggestMatching(playerManager.getPlayerList().stream().filter((player) -> {
-                return !playerManager.isOperator(player.getGameProfile());
-            }).map((player) -> {
-                return player.getGameProfile().getName();
-            }), builder);
-        }).executes((context) -> {
-            return op(context.getSource(), GameProfileArgumentType.getProfileArgument(context, "targets"));
-        })));
+    public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("reloadngroklanlists")
+                .requires(source -> source.hasPermissionLevel(3))
+                .executes(ReloadJsonListsCommand::loadJson));
     }
 
+    private static int loadJson(CommandContext<ServerCommandSource> serverCommandSourceCommandContext) throws CommandSyntaxException {
+        ServerCommandSource source = serverCommandSourceCommandContext.getSource();
 
-    private boolean loadJson(ServerCommandSource source){
         int i;
         boolean bl3 = false;
 
         for(i = 0; !bl3 && i <= 2; ++i) {
             if (i > 0) {
                 NgrokLan.LOGGER.warn("Encountered a problem while converting the op list, retrying in a few seconds");
-                this.sleepFiveSeconds();
+                ReloadJsonListsCommand.sleepFiveSeconds();
             }
 
             bl3 = ServerConfigHandler.convertOperators(source.getServer());
@@ -55,20 +53,27 @@ public class ReloadJsonListsCommand {
         for(i = 0; !bl4 && i <= 2; ++i) {
             if (i > 0) {
                 NgrokLan.LOGGER.warn("Encountered a problem while converting the whitelist, retrying in a few seconds");
-                this.sleepFiveSeconds();
+                ReloadJsonListsCommand.sleepFiveSeconds();
             }
 
             bl4 = ServerConfigHandler.convertWhitelist(source.getServer());
             //fail is false
         }
 
-        return bl3 && bl4;
+        if(! (bl3 && bl4) ){
+            throw LOAD_JSON_EXCEPTION.create();
+        }
+
+        return 1;
     }
 
-    private void sleepFiveSeconds() {
+
+
+
+    private static void sleepFiveSeconds() {
         try {
             Thread.sleep(5000L);
-        } catch (InterruptedException var2) {
+        } catch (InterruptedException ignored) {
         }
     }
 }
